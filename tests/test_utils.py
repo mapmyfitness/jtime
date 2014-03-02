e0dfd0650bee5adb1dba0586eda811aa91f82296
Brett Nekolny <brett.nekolny@mapmyfitness.com>
2014-03-02 14:52:20 -0700

    Adding tests around timedelta to total seconds method.

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,4 +1,5 @@
 """ Tests for jtime utils module """
+import datetime
 import mock
 import sys
 if sys.version_info < (2, 7):
@@ -23,3 +24,13 @@ class JtimeUtilsTestCase(unittest.TestCase):
     def test_get_input_bad_input(self):
         input_func = mock.Mock(side_effect=["", None, "input!"])
         utils.get_input(input_func, "Test to get input!")
+
+    def test_timedelta_total_seconds(self):
+        one_day = datetime.timedelta(1)  # 1 day
+        one_day_sec = 60 * 60 * 24
+
+        one_hr = datetime.timedelta(hours=1)
+        one_hr_sec = 60 * 60
+
+        self.assertEquals(utils.timedelta_total_seconds(one_day), one_day_sec)
+        self.assertEquals(utils.timedelta_total_seconds(one_hr), one_hr_sec)
