72930fce4b8d6528b4dff09899824366beea5776
Brett Nekolny <brett.nekolny@mapmyfitness.com>
2014-03-02 21:23:16 -0700

    Adding invalid git repo test to the git_ext tests.

--- tests/test_git_ext.py
+++ tests/test_git_ext.py
@@ -1,5 +1,11 @@
 """ Tests for the jtime git_ext module. """
-import unittest
+import git
+import mock
+import sys
+if sys.version_info < (2, 7):
+    import unittest2 as unittest
+else:
+    import unittest
 
 from jtime import git_ext
 
@@ -13,3 +19,9 @@ class JtimeGitTestCase(unittest.TestCase):
 
     def test_branch(self):
         self.assertNotEqual(self.repo.branch, None)
+
+    def test_branch_raises_InvalidGitRepositoryError(self):
+        type(self.repo).active_branch = mock.PropertyMock(
+            side_effect=git.errors.InvalidGitRepositoryError
+        )
+        self.repo.branch
