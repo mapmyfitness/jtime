a1d304ec0a39d0bf1ec2a47ab05c800dba28a666
Brett Nekolny <brett.nekolny@mapmyfitness.com>
2014-03-12 23:48:00 -0600

    Setting a version number, greater than what we had internally.

--- jtime/__init__.py
+++ jtime/__init__.py
@@ -3,4 +3,4 @@ __import__('pkg_resources').declare_namespace(__name__)
 __description__ = "Jira time tracking tool built on the command line to take context from your various git repositories."
 __author__ = 'MapMyFitness'
 __url__ = 'http://github.com/mapmyfitness/jtime'
-__version__ = ''
+__version__ = '0.5'
